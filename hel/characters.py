f4ed6c67bda99d8c123b5216f56e968b807302b6
sp1keyman <shutout3030@gmail.com>
2025-01-29 14:20:33 -0500

    added a return for an array that contains a  randomly generated enemy

--- hel/characters.py
+++ hel/characters.py
@@ -39,18 +39,31 @@ def enemy_generator():
     e_name = enemy[gen_enemy_id]#assignes the value of the array at gen_enemy_id to e_name
     
     rarity = rarity_generator()#calls rarity of first assigned item
-    e_item_off_name = list(items["offensive"][rarity])[1]#list function will create a list of items in the rarity that was assigned to this and then pick the one in the second slot with the [1]. This will be the key not the value for some reason.
+    e_item_off_name = list(items["offensive"][rarity])[1]#list function will create a list of items in the rarity that was assigned to this and then pick the one in the second slot with the [1]. This will be the key not the value because we are choosing from a list
     e_item_off_atk = items["offensive"][rarity][e_item_off_name]#need to get the value now 
 
     rarity = rarity_generator()#calls rarity again to get new value for second assigned item
-    e_item_def_name = list(items["defensive"][rarity])[1]#list function will create a list of items in the rarity that was assigned to this and then pick the one in the second slot with the [1]. This will be the key not the value for some reason
+    e_item_def_name = list(items["defensive"][rarity])[1]#list function will create a list of items in the rarity that was assigned to this and then pick the one in the second slot with the [1]. This will be the key not the value becasue we are choosing from a list
     e_item_def_def = items["defensive"][rarity][e_item_def_name]#need to get the value now 
 
     e_attk = 0 + e_item_off_atk #placeholder in  case I want to add base stats to the enemies later
     e_def = 0+ e_item_def_def #placeholder in case i want to add base stats to the enemies later
 
+    #store enemy values in an array that we can import to the main fuction? (idk if this will be useful or not) **creates a dict with enemy name [key(off name):value(off atk), key(def name):value(def def)]
+    e1 = {
+        e_name : { e_item_off_name : e_item_off_atk, 
+                  e_item_def_name : e_item_def_def
+                }
+    }
     
-    print(f"As you enter the hall you encounter a {e_name} with an attack of {e_attk} and a defense of {e_def}") #may move this over to the main.py at some point. just print values right now.
+    print(f"As you enter the hall you encounter a {e_name} weilding a {e_item_off_name} with an attack of {e_attk} and wearing a {e_item_def_name} with a defense of {e_def}\n") #may move this over to the main.py at some point. just print values right now.
+
+    return e1 #returns a (almost) fully randomly generated enemy 
+
+
+
+
+
 def rarity_generator():
     rarity = random.randint(1,100)
     if rarity <= 100 and rarity >=95:
