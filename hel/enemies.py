85af5b9e73954cc0af2eb2870e91b7aae3cef284
sp1keyman <shutout3030@gmail.com>
2025-01-28 22:35:13 -0500

    added comment to enemies.py

--- hel/enemies.py
+++ hel/enemies.py
@@ -7,4 +7,7 @@ enemy = [
 
 #generates a random enemy from list of enemy names
 def enemy_generator():
-    print(f"As you enter the hall you encounter a {enemy[random.randint(0,len(enemy)-1)]}") # the random.randint is calling a random interger and the (0,len(enemy)-1) means choose random int from 0 to lenth of enemy array -1 *so essentially index randomly at 0,1,2,3
+    
+    # the random.randint is calling a random interger and the (0,len(enemy)-1) means choose random int from 0 to lenth of enemy array -1        *so essentially index randomly at 0, 1, 2, or 3 and return the value
+    print(f"As you enter the hall you encounter a {enemy[random.randint(0,len(enemy)-1)]}") 
+
